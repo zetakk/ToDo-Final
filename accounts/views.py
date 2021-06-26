0edc32699265d7927c93e40edd53ae331c39276f
nukiska <g.nina@seznam.cz>
2021-06-26 15:39:49 +0200

    do accounts/views.py přidaný register, login, logout a changepassword view

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.views import LoginView, PasswordChangeView, LogoutView
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
 
-# Create your views here.
+
+class RegisterUserView(CreateView):
+    template_name = 'form.html'
+    form_class = UserCreationForm
+    success_url = reverse_lazy('all-tasks')
+
+
+class UserLoginView(LoginView):
+    template_name = 'form.html'
+
+
+class UserLogoutView(LogoutView):
+    success_url = reverse_lazy('login')
+
+
+class UserPasswordChangeView(PasswordChangeView):
+    template_name = 'form.html'
+    success_url = reverse_lazy('all-tasks')
