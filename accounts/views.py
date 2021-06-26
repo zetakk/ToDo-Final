41ccb87c6739289dadb5e808ed9a85e489cb4aaa
nukiska <g.nina@seznam.cz>
2021-06-26 20:43:47 +0200

    do views.py pridany autologin po registraci

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,7 @@
+from django.contrib.auth import authenticate, login
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.views import LoginView, PasswordChangeView, LogoutView
+from django.shortcuts import redirect
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
 
@@ -8,6 +10,18 @@ class RegisterUserView(CreateView):
     template_name = 'form.html'
     form_class = UserCreationForm
     success_url = reverse_lazy('all-tasks')
+
+    # auto login after register:
+    def form_valid(self, form):
+        # save the new user first
+        form.save()
+        # get the username and password
+        username = self.request.POST['username']
+        password = self.request.POST['password1']
+        # authenticate user then login
+        user = authenticate(username=form.cleaned_data['username'], password=form.cleaned_data['password1'], )
+        login(self.request, user)
+        return redirect(self.success_url)
 
 
 class UserLoginView(LoginView):
