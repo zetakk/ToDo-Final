c448300d7ee1313c1ba8173cec8ed90771d02062
nukiska <g.nina@seznam.cz>
2021-06-26 15:32:14 +0200

    přidaná cesta do core/urls.py, nastavený accounts/urls.py

---
+++ accounts/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+
+from accounts.views import RegisterUserView, UserLoginView, UserLogoutView, UserPasswordChangeView
+
+urlpatterns = [
+    path('register-user/', RegisterUserView.as_view(), name='register-user'),
+    path('login/', UserLoginView.as_view(), name='login'),
+    path('logout/', UserLogoutView.as_view(), name='logout'),
+    path('password-change/', UserPasswordChangeView.as_view(), name='password-change'),
+]
