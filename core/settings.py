af808b3610afbecfb44cf1112e36071f9a7014a9
nukiska <g.nina@seznam.cz>
2021-06-26 15:45:27 +0200

    kosmetické úpravy

--- core/settings.py
+++ core/settings.py
@@ -4,7 +4,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
@@ -15,7 +14,6 @@ SECRET_KEY = 'django-insecure-o!r5y#j#&a7x-3#)6fkg$8=q+tpcq334w6u*vto1pj=hskxmu6
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -60,7 +58,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'core.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
@@ -70,7 +67,6 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
@@ -90,7 +86,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
@@ -103,7 +98,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
--- todo/views.py
+++ todo/views.py
@@ -5,6 +5,7 @@ from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 
+
 class AllTasks(ListView):
     model = Task
     template_name = 'index.html'
@@ -12,13 +13,13 @@ class AllTasks(ListView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['count_no'] = context['all_tasks'].filter(completed = False).count()
+        context['count_no'] = context['all_tasks'].filter(completed=False).count()
         context['count_yes'] = context['all_tasks'].filter(completed=True).count()
         context['count_all'] = context['all_tasks'].count()
 
         search = self.request.GET.get('searchbar') or ''
         if search:
-            context['all_tasks'] = context['all_tasks'].filter(name__icontains = search)
+            context['all_tasks'] = context['all_tasks'].filter(name__icontains=search)
 
         context['search'] = search
 
@@ -44,6 +45,7 @@ class DeleteTask(DeleteView):
     success_url = reverse_lazy('all-tasks')
     template_name = 'delete_task.html'
     context_object_name = 'task'
+
 
 class DetailTask(DetailView):
     model = Task
