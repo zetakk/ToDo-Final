a785f3187ac177ffa8cc340296cb6f715bf73bad
nukiska <g.nina@seznam.cz>
2021-06-27 03:21:54 +0200

    pridane soubory pro heroku

--- core/settings.py
+++ core/settings.py
@@ -1,5 +1,7 @@
 import os.path
 from pathlib import Path
+import django_heroku
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -102,3 +104,5 @@ LOGOUT_REDIRECT_URL = 'all-tasks'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+django_heroku.settings(locals())
